c72707defa4f4bd1498b6c14dd8ba8b129662a21
MosabUI <mossap100@gmail.com>
2022-12-16 08:26:51 +0200

    final project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import Home from './pages/Home'
+import Hotel from './pages/Hotel'
+import List from './pages/List'
+import SingleHotel from './pages/SingleHotel'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+           <Route  path='/' element={<Home/>}  />
+           <Route  path='/hotel' element={<Hotel/>}  />
+           <Route  path='/hotel/:id' element={<SingleHotel/>}  />
+        </Routes>
+      </Router>
     </div>
   );
 }
